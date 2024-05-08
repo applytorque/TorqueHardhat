df5948b0dc09f16f1c323d941fa5fb3b693d2c60
tushar <torquedapp@gmail.com>
2024-05-08 10:48:12 +0530

    Hardhat Setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,40 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
 
 // Ensure your configuration variables are set before executing the script
 const { vars } = require("hardhat/config");
 
 // The RPC creds
-const IPSPROTOCOL_API_KEY = vars.get("IPSPROTOCOL_API_KEY");
+const AMOY_API_KEY = vars.get("AMOY_API_KEY");
 
-// Add your IPSChain account private key to the configuration variables
-// To export your private key from Coinbase Wallet, go to
-// Settings > Developer Settings > Show private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-// IPSPROTOCOL_PRIVATE_KEY
-const IPSPROTOCOL_PRIVATE_KEY = vars.get("IPSPROTOCOL_PRIVATE_KEY");
+// Add your amoy account private key to the configuration variables
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    ipschain: {
-      url: `https://${IPSPROTOCOL_API_KEY}:${IPSPROTOCOL_API_KEY}@ipschain.ipsprotocol.xyz`,
-      accounts: [IPSPROTOCOL_PRIVATE_KEY],
+    amoy: {
+      url: `https://rpc-amoy.polygon.technology/`,
+      accounts: [PRIVATE_KEY],
+      gasPrice: 10000000000, // 10 Gwei, adjust this value as necessary
     },
   },
   etherscan: {
     apiKey: {
-      ipschain: "123456789"
+      amoy: "364892..."
     },
-     customChains: [
+    customChains: [
       {
-        network: "ipschain",
-        chainId: 8337,
+        network: "amoy",
+        chainId: 80002,
         urls: {
-          apiURL: "https://explorer.ipsprotocol.xyz/api",
-          browserURL: "https://explorer.ipsprotocol.xyz"
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com/"
         }
       }
     ]
